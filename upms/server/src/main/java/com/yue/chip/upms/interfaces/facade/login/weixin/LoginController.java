a7c6c510f740989a50f4900404c9c013217ac3a7
Mr.Liu <158442534@qq.com>
2024-02-19 09:38:25 +0800

    ^慢慢完善……^

--- upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/weixin/LoginController.java
+++ upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/weixin/LoginController.java
@@ -4,11 +4,7 @@ import com.yue.chip.annotation.AuthorizationIgnore;
 import com.yue.chip.core.IResultData;
 import com.yue.chip.core.ResultData;
 import com.yue.chip.upms.domain.service.login.LoginService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import javax.annotation.Resource;
-import javax.validation.constraints.NotBlank;
 import lombok.extern.java.Log;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.validation.constraints.NotBlank;
 import java.util.HashMap;
 import java.util.Map;
 
