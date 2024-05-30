867554e15c80ba8a1504e8b9ab561fa5cd587b0d
chengyongjie <294372602@qq.com>
2024-05-30 09:31:52 +0800

    修改网格

--- upms/frontend/registerGateway.js
+++ upms/frontend/registerGateway.js
@@ -2,7 +2,7 @@ const request = require("request");
 const registerGateway = function registerGateway() {
     request('http://192.168.31.111/register/gateway?port=5177&register_host=192.168.31.189', function (error, response, body) {
         if (error) {
-            console.log(error);
+            console.log(error); 
         }
     });
 };
