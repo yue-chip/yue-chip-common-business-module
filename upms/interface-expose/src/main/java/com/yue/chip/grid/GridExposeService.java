374fa36240f4fedcdb07ed6811b69833bc8cda1e
chen1 <1562090524@qq.com>
2024-07-11 10:12:28 +0800

    新增远程调用删除网格接口

--- upms/interface-expose/src/main/java/com/yue/chip/grid/GridExposeService.java
+++ upms/interface-expose/src/main/java/com/yue/chip/grid/GridExposeService.java
@@ -1,9 +1,15 @@
 package com.yue.chip.grid;
 
+import com.yue.chip.core.IResultData;
 import com.yue.chip.core.PageSerializable;
+import com.yue.chip.core.ResultData;
 import com.yue.chip.core.YueChipPage;
 import com.yue.chip.grid.vo.GridExposeVo;
 import com.yue.chip.upms.vo.UserExposeVo;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Set;
@@ -31,4 +37,10 @@ public interface GridExposeService {
     PageSerializable<GridExposeVo> listGridQuery(Set<Long> organizationalIds, String name, YueChipPage yueChipPage, Set<Long> userIds, String time);
 
     PageSerializable<UserExposeVo> findByGridIdIn(Set<Long> gridIds, String name, YueChipPage yueChipPage);
+
+    /**
+     * 删除网格
+     * @param ids
+     */
+    void deleteGrid(@NotNull @Size(min = 0) List<Long> ids);
 }
