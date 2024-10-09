c85bda78656911e707b1041fcdba9d99c1c1fd96
LiaoJiaCheng <1227444365@qq.com>
2024-10-09 18:22:58 +0800

    添加英文名称字段

--- upms/common/src/main/java/com/yue/chip/upms/definition/resources/ResourcesDefinition.java
+++ upms/common/src/main/java/com/yue/chip/upms/definition/resources/ResourcesDefinition.java
@@ -43,6 +43,9 @@ public class ResourcesDefinition extends BaseDefinition {
     @Schema(description = "资源名称")
     private String name;
 
+    @Schema(description = "资源英文名称")
+    private String nameEn;
+
     @Schema(description = "作用域-(code:"+Scope.code+",version:"+Scope.version+")")
     @Builder.Default
     private Scope scope = Scope.CONSOLE;
