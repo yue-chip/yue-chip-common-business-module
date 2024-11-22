7e0fd9b33ff4a5edad961a76604936b6ef24d910
chenyongjie <chenyongjie@qq.com>
2024-11-22 10:17:04 +0800

    完成上传菜单图标

--- upms/frontend/registerGateway.js
+++ upms/frontend/registerGateway.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const registerGateway = function registerGateway() {
-    request('http://192.168.11.218/register/gateway?port=5177&register_host=192.168.11.118', function (error, response, body) {
+    request('http://192.168.11.172/register/gateway?port=5177&register_host=192.168.11.120', function (error, response, body) {
         if (error) {
             console.log(error); 
         }
