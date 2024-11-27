954df08ae1ffd2e2913a573d01528a3086e7fb8d
LiaoJiaCheng <1227444365@qq.com>
2024-11-27 13:54:29 +0800

    优化

--- upms/server/src/main/java/com/yue/chip/upms/infrastructure/repository/organizational/impl/OrganizationalRepositoryImpl.java
+++ upms/server/src/main/java/com/yue/chip/upms/infrastructure/repository/organizational/impl/OrganizationalRepositoryImpl.java
@@ -531,7 +531,7 @@ public class OrganizationalRepositoryImpl implements OrganizationalRepository {
                     userAddOrUpdateDto.setPhoneNumber(phone);
                     List<Long> organizationalIds = new ArrayList<>();
                     organizationalIds.add(userOrganizationalIdMap.get(user));
-                    userAddOrUpdateDto.setOrganizationalIds(organizationalIds);
+                    userAddOrUpdateDto.setOrganizationalId(organizationalIds);
                     upmsApplication.saveUser(userAddOrUpdateDto);
                 });
             }
