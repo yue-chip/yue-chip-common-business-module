35417254a0321b09735bbb889af61b72ff0f544e
LiaoJiaCheng <1227444365@qq.com>
2024-11-27 10:56:25 +0800

    优化

--- upms/server/src/main/java/com/yue/chip/upms/interfaces/dto/user/UserAddOrUpdateDto.java
+++ upms/server/src/main/java/com/yue/chip/upms/interfaces/dto/user/UserAddOrUpdateDto.java
@@ -1,5 +1,10 @@
 package com.yue.chip.upms.interfaces.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.yue.chip.core.persistence.Validator;
 import com.yue.chip.upms.definition.user.UserDefinition;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,6 +15,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -25,7 +31,7 @@ public class UserAddOrUpdateDto extends UserDefinition {
 
 //    @NotBlank(message = "组织机构ID不能为空",groups = {Validator.Insert.class,Validator.Update.class})
     @Schema(description = "组织机构ID")
-    private List<Long> organizationalId;
+    private List<Long> organizationalIds;
 
     @NotBlank(message = "密码不能为空",groups = {Validator.Insert.class})
     @Schema(description = "密码")
@@ -53,6 +59,22 @@ public class UserAddOrUpdateDto extends UserDefinition {
 //    @NotBlank(message = "电话号码不能为空",groups = {Validator.Update.class,Validator.Insert.class})
     public String getPhoneNumber() {
         return super.getPhoneNumber();
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public LocalDateTime getCreateDateTime() {
+        return super.getCreateDateTime();
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public LocalDateTime getLastLoginTime() {
+        return super.getLastLoginTime();
     }
 
 }
