452307dd6f727ed3fc1ac3919f4edde3fe31e37f
LiaoJiaCheng <1227444365@qq.com>
2025-01-08 13:52:17 +0800

    登录日志bug（未修复）

--- upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/pc/LoginController.java
+++ upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/pc/LoginController.java
@@ -1,12 +1,16 @@
 package com.yue.chip.upms.interfaces.facade.login.pc;
 
 import cn.hutool.core.codec.Base64;
+import com.security.log.LogExposeService;
 import com.yue.chip.annotation.AuthorizationIgnore;
 import com.yue.chip.annotation.SystemLog;
 import com.yue.chip.core.IResultData;
 import com.yue.chip.core.ResultData;
 import com.yue.chip.upms.application.service.UpmsApplication;
 import com.yue.chip.upms.domain.service.login.LoginService;
+import com.yue.chip.upms.infrastructure.dao.user.UserDao;
+import com.yue.chip.upms.infrastructure.dao.weixin.UserWeiXinDao;
+import com.yue.chip.upms.infrastructure.po.user.UserPo;
 import com.yue.chip.upms.interfaces.dto.user.UserAddOrUpdateDto;
 import com.yue.chip.utils.CurrentUserUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,6 +19,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotBlank;
 import lombok.extern.java.Log;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Mr.Liu
@@ -41,23 +48,36 @@ public class LoginController{
     @Resource
     private UpmsApplication upmsApplication;
 
+    @DubboReference
+    private LogExposeService logExposeService;
+
+    @Resource
+    private UserDao userDao;
 
     @PostMapping("/login1")
     @AuthorizationIgnore
     @Operation(summary = "登录", description = "登录")
-    @SystemLog(value = "登录账号")
     public IResultData<String> login(@NotBlank(message = "登录账号不能为空") @Parameter(description = "登录账号",name = "username",required = true)String username,
                                      @NotBlank(message = "密码不能为空") @Parameter(description = "密码",name = "password",required = true)String password) {
         String token = loginService.login(username,password);
         Map<String,String> map = new HashMap<>();
         map.put("token",token);
+        Optional<UserPo> firstByUsername = userDao.findFirstByUsername(username);
+        try {
+            if (firstByUsername.isPresent()) {
+                logExposeService.saveLogId("登录账号", firstByUsername.get().getId(), "pc");
+            }
+        } catch (Exception e) {
+            System.out.println("---------------");
+            System.out.println(e.getMessage());
+            System.out.println("---------------");
+        }
         return ResultData.builder().data(map).build();
     }
 
     @PostMapping("/login2")
     @AuthorizationIgnore
     @Operation(summary = "登录", description = "登录")
-    @SystemLog(value = "登录账号")
     public IResultData<String> login2(String data) {
         CurrentUserUtil.setCurrentTenantNumber("48");
         String str = Base64.decodeStr(data);
@@ -69,15 +89,25 @@ public class LoginController{
         Map<String,String> map = new HashMap<>();
         map.put("token",token);
         CurrentUserUtil.cleanCurrentTenantNumber();
+        Optional<UserPo> firstByUsername = userDao.findFirstByUsername(username);
+        try {
+            if (firstByUsername.isPresent()) {
+                logExposeService.saveLogId("登录账号", firstByUsername.get().getId(), "pc");
+            }
+        } catch (Exception e) {
+            System.out.println("---------------");
+            System.out.println(e.getMessage());
+            System.out.println("---------------");
+        }
         return ResultData.builder().data(map).build();
     }
 
     @GetMapping("/login/out")
     @AuthorizationIgnore
-    @SystemLog(value = "退出登录")
     @Operation(summary = "退出登录", description = "退出登录")
     public IResultData<String> loginOut() {
         loginService.loginOut();
+        logExposeService.saveLog("退出登录");
         return ResultData.builder().build();
     }
 
