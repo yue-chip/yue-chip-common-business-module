bd2e5f7cb514c2b286e668dedcaf846a823a17e9
Mr.Liu <158442534@qq.com>
2025-02-13 11:27:45 +0800

    优化

--- upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/pc/LoginController.java
+++ upms/server/src/main/java/com/yue/chip/upms/interfaces/facade/login/pc/LoginController.java
@@ -3,25 +3,20 @@ package com.yue.chip.upms.interfaces.facade.login.pc;
 import cn.hutool.core.codec.Base64;
 import com.security.log.LogExposeService;
 import com.yue.chip.annotation.AuthorizationIgnore;
-import com.yue.chip.annotation.SystemLog;
 import com.yue.chip.core.IResultData;
 import com.yue.chip.core.ResultData;
 import com.yue.chip.core.SystemLogService;
 import com.yue.chip.upms.application.service.UpmsApplication;
 import com.yue.chip.upms.domain.service.login.LoginService;
 import com.yue.chip.upms.infrastructure.dao.user.UserDao;
-import com.yue.chip.upms.infrastructure.dao.weixin.UserWeiXinDao;
 import com.yue.chip.upms.infrastructure.po.user.UserPo;
 import com.yue.chip.upms.interfaces.dto.user.UserAddOrUpdateDto;
-import com.yue.chip.utils.CurrentUserUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotBlank;
 import lombok.extern.java.Log;
-import org.apache.dubbo.config.annotation.DubboReference;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,7 +75,7 @@ public class LoginController{
     @AuthorizationIgnore
     @Operation(summary = "登录", description = "登录")
     public IResultData<String> login2(String data) {
-        CurrentUserUtil.setCurrentTenantNumber("48");
+//        CurrentUserUtil.setCurrentTenantNumber("48");
         String str = Base64.decodeStr(data);
         String username = str.split("&")[1].split("=")[1];
         String name = str.split("&")[2].split("=")[1];
@@ -89,7 +84,7 @@ public class LoginController{
         String token = loginService.login(username,password);
         Map<String,String> map = new HashMap<>();
         map.put("token",token);
-        CurrentUserUtil.cleanCurrentTenantNumber();
+//        CurrentUserUtil.cleanCurrentTenantNumber();
         Optional<UserPo> firstByUsername = userDao.findFirstByUsername(username);
         try {
             if (firstByUsername.isPresent()) {
