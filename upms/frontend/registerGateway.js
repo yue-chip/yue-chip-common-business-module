259b70f4e737449f59a9e0490d4e3b8554345f8e
chenyongjie <chenyongjie@qq.com>
2025-02-17 14:01:24 +0800

    修改租户用户网格等

--- upms/frontend/registerGateway.js
+++ upms/frontend/registerGateway.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const registerGateway = function registerGateway() {
-    request('http://192.168.11.172/register/gateway?port=5177&register_host=192.168.11.126', function (error, response, body) {
+    request('http://192.168.11.172/register/gateway?port=5177&register_host=192.168.11.125', function (error, response, body) {
         if (error) {
             console.log(error); 
         }
