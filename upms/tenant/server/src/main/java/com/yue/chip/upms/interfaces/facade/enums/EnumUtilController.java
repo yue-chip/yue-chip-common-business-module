69e51ae2c4dac59a5affafb7f2c642c889c3bb6a
Mr.Liu <158442534@qq.com>
2025-06-18 16:49:33 +0800

    修改

--- upms/tenant/server/src/main/java/com/yue/chip/upms/interfaces/facade/enums/EnumUtilController.java
+++ upms/tenant/server/src/main/java/com/yue/chip/upms/interfaces/facade/enums/EnumUtilController.java
@@ -2,16 +2,24 @@ package com.yue.chip.upms.interfaces.facade.enums;
 
 import com.yue.chip.core.IResultData;
 import com.yue.chip.core.ResultData;
+import com.yue.chip.core.common.enums.EnumPersistenceBean;
+import com.yue.chip.core.common.remote.http.RemoteEnumPersistenceHttp;
+import com.yue.chip.upms.assembler.enums.EnumUtilMapper;
+import com.yue.chip.upms.domain.repository.enums.EnumUtilRepository;
 import com.yue.chip.upms.interfaces.vo.enums.EnumUtilVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotBlank;
 import lombok.extern.java.Log;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Mr.Liu
@@ -22,8 +30,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @Tag(name = "枚举")
 @Log
-public class EnumUtilController {
+public class EnumUtilController implements RemoteEnumPersistenceHttp {
 
+    @Resource
+    private EnumUtilRepository enumUtilRepository;
+
+    @Resource
+    private EnumUtilMapper enumUtilMapper;
 
     @GetMapping("")
     @Operation(description = "获取枚举",summary = "获取枚举")
@@ -34,5 +47,14 @@ public class EnumUtilController {
 //            return ResultData.builder().data(enumUtilMapper.toEnumUtilVo(optional.get())).build();
 //        }
         return ResultData.builder().build();
+    }
+
+    @Override
+    public void save(List<EnumPersistenceBean> list) {
+        if (Objects.nonNull(list) && !list.isEmpty()) {
+            list.forEach(bean -> {
+                enumUtilRepository.save(enumUtilMapper.toEnumUtilPo(bean));
+            });
+        }
     }
 }
