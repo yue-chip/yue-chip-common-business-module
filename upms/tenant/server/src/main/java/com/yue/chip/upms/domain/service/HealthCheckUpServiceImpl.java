8e77575a23152a817515ca2984d4e6a1a0d5f389
Mr.Liu <158442534@qq.com>
2025-09-13 11:40:34 +0800

    升级优化

---
+++ upms/tenant/server/src/main/java/com/yue/chip/upms/domain/service/HealthCheckUpServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yue.chip.upms.domain.service;
+
+import com.yue.chip.core.health.HealthCheckUpService;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Server
+public class HealthCheckUpServiceImpl implements HealthCheckUpService {
+    @Override
+    public Boolean checkHealth() {
+        return true;
+    }
+}
