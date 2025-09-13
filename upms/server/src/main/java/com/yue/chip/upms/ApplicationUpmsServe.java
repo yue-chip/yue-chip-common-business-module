0761bc61c329dc3dbfe2f2ac9f721fbf614ed679
Mr.Liu <158442534@qq.com>
2025-09-13 10:17:52 +0800

    升级优化

--- upms/server/src/main/java/com/yue/chip/upms/ApplicationUpmsServe.java
+++ upms/server/src/main/java/com/yue/chip/upms/ApplicationUpmsServe.java
@@ -4,19 +4,16 @@ import com.yue.chip.annotation.HttpExchangeScan;
 import com.yue.chip.annotation.HttpExchangeScans;
 import com.yue.chip.core.persistence.BaseDaoFactoryBean;
 import com.yue.chip.security.SecurityConfig;
-import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.restart.RestartEndpoint;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication (exclude = {SecurityConfig.class})
 @ComponentScan(basePackages = "com.yue.chip.**")
@@ -30,9 +27,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 @HttpExchangeScans({@HttpExchangeScan("com.yue.chip.**")})
 public class ApplicationUpmsServe {
 
-    @Resource
-    RestartEndpoint restartEndpoint;
-
     public static void main ( String args[] ) throws Exception {
         /*
          * new SpringApplicationBuilder(Application.class)
@@ -40,10 +34,5 @@ public class ApplicationUpmsServe {
          */
         SpringApplication.run(ApplicationUpmsServe.class, args);
 //        System.out.println(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(SecureUtil.md5("admin")));
-    }
-
-    @Scheduled(cron = "*/6 * * * * ?")
-    public void sayHello() {
-        restartEndpoint.doRestart();
     }
 }
